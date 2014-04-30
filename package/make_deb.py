e1d67abdcad9c6aba895ac7f083c55622ea96990
mobyfab <mobyfab@free.fr>
2014-04-30 12:52:54 +0200

    Updated package script.

--- package/make_deb.py
+++ package/make_deb.py
@@ -4,88 +4,98 @@ import sys,os, re, argparse, iso8601, shutil
 from subprocess import call, check_output, Popen
 from xml.etree import ElementTree as ET
 
-releases = ["precise", "quantal", "raring", "saucy", "trusty"]
+releases = ["precise", "quantal","saucy", "trusty"]
+build_for = []
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--release', help='The Ubuntu release')
+parser.add_argument('-a', '--all', help='Build for all releases', action="store_true")
 parser.add_argument('-s', '--source', help='Build signed source package', action="store_true")
 parser.add_argument('-b', '--bin', help='Build local binary package', action="store_true")
 parser.add_argument('-p', '--ppa', help='Send source package to PPA', action="store_true")
 
 def makeChangelog(release, dest, rev):
 
-	#~ f = open(dest+"/debian/changelog", "r")
-	#~ tmp = f.read()
-	#~ f.close()
-	
-	pass
-	
-def copySrc(dest, rev):
-	
-	check_output(["rm -rf "+dest], shell=True)
-	print check_output(["mkdir -v "+dest], shell=True)
+  #~ f = open(dest+"/debian/changelog", "r")
+  #~ tmp = f.read()
+  #~ f.close()
+  
+  pass
+  
+def copySrc(release, dest, rev):
+  
+  check_output(["rm -rf "+dest], shell=True)
+  print check_output(["mkdir -v "+dest], shell=True)
 
-	#~ print check_output(["shopt -s extglob; cp -r ../!(package) "+dest+"/"], shell=True)
-	cmd = "find .. -maxdepth 1 \! \( -name package -o -name *.pro.user* -o -name .. \) -exec cp -r '{}' "+dest+"/ ';'"
-	check_output([cmd], shell=True)
-	
-	print check_output(["cd "+dest+"; echo "" | dh_make -n --single -e fabien.poussin@gmail.com -c gpl3"], shell=True)
+  #~ print check_output(["shopt -s extglob; cp -r ../!(package) "+dest+"/"], shell=True)
+  cmd = "find .. -maxdepth 1 \! \( -name package -o -name *.pro.user* -o -name .. \) -exec cp -r '{}' "+dest+"/ ';'"
+  check_output([cmd], shell=True)
+  
+  print check_output(["cd "+dest+"; echo "" | dh_make -n --single -e fabien.poussin@gmail.com -c gpl3"], shell=True)
 
-	print check_output(["cp -v debian/* "+dest+"/debian/"], shell=True)
-	check_output(["rm -f "+dest+"/debian/*.ex "+dest+"/debian/*.EX "+dest+"/debian/ex.*"], shell=True)
-	
-	f = open(dest+"/debian/changelog", "r")
-	tmp = f.read().replace("unstable; urgency=low", args.release+"; urgency=medium")
-	f.close()
-	f = open(dest+"/debian/changelog", "w")
-	f.write(str(tmp))
-	f.close()
-	
-	f = open(dest+"/version", "w")
-	f.write(str(rev))
-	f.close()
-	
+  print check_output(["cp -v debian/* "+dest+"/debian/"], shell=True)
+  check_output(["rm -f "+dest+"/debian/*.ex "+dest+"/debian/*.EX "+dest+"/debian/ex.*"], shell=True)
+  
+  f = open(dest+"/debian/changelog", "r")
+  tmp = f.read().replace("unstable; urgency=low", release+"; urgency=medium")
+  f.close()
+  f = open(dest+"/debian/changelog", "w")
+  f.write(str(tmp))
+  f.close()
+  
+  f = open(dest+"/version", "w")
+  f.write(str(rev))
+  f.close()
+  
 def makeSrc(dest):
-	print "Building Source package"
-	print check_output(["cd "+dest+"; debuild -j4 -S -sa"], shell=True)
-	
+  print "Building Source package"
+  print check_output(["cd "+dest+"; debuild -j4 -S -sa"], shell=True)
+  
 def makeBin(dest):
-	print "Building binary package"
-	print check_output(["cd "+dest+"; debuild -j4 -b -uc -us"], shell=True)
+  print "Building binary package"
+  print check_output(["cd "+dest+"; debuild -j4 -b -uc -us"], shell=True)
 
 def sendSrc(ver):
-	print check_output(["dput ppa:mobyfab/qstlink2 "+ver+"_source.changes"], shell=True)
+  print check_output(["dput ppa:mobyfab/qstlink2 "+ver+"_source.changes"], shell=True)
 
 if __name__ == "__main__":
 
-	args = parser.parse_args()
-	
-	if not args.release:
-		parser.print_help()
+  args = parser.parse_args()
+  
+  if (not args.release and not args.all) or (not args.source and not args.bin):
+    parser.print_help()
+    sys.exit(1)
 
-	if args.release not in releases:
-		print "Invalid Ubuntu release, please chose among:", releases
-		sys.exit(1)
-		
-	ver = check_output(["grep \"VERSION =\" ../QStlink2.pro | awk '{ print $3 }' "], shell=True).replace('\n','')
-	if not ver:
-		print "Could not fetch last version"
-		sys.exit(1)
+  if not args.all and args.release not in releases:
+    print "Invalid Ubuntu release, please chose among:", ", ".join(releases)
+    sys.exit(1)
+  
+  if args.all:
+    build_for = releases[:]
+  else:
+    build_for.append(args.release)
+    
+  ver = check_output(["grep \"VERSION =\" ../QStlink2.pro | awk '{ print $3 }' "], shell=True).replace('\n','')
+  if not ver:
+    print "Could not fetch last version"
+    sys.exit(1)
 
-	print "Last version:" , ver
-	
-	folder_name = 'qstlink2-'+str(ver)+'~'+args.release
-	print "Package version:" , folder_name
-	
-	copySrc(folder_name, ver)
-	makeChangelog(args.release, folder_name, ver)
-	
-	if args.source:
-		makeSrc(folder_name)
-		if args.ppa:
-			sendSrc('qstlink2_'+str(ver)+'~'+args.release)
-	if args.bin:
-		makeBin(folder_name)
-		
-	print check_output(["rm -rf "+folder_name], shell=True)
-	
+  print "Last version:" , ver
+  
+  for release in build_for:
+  
+    folder_name = 'qstlink2-'+str(ver)+'~'+release
+    print "Package version:" , folder_name
+    
+    copySrc(release, folder_name, ver)
+    makeChangelog(release, folder_name, ver)
+    
+    if args.source:
+      makeSrc(folder_name)
+      if args.ppa:
+        sendSrc('qstlink2_'+str(ver)+'~'+release)
+    if args.bin:
+      makeBin(folder_name)
+      
+    print check_output(["rm -rf "+folder_name], shell=True)
+  
